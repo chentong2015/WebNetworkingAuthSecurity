d69c1880b6f98d6a5f2c69a993a6077e29ddf08e
tchen <tchen@murex.com>
2022-06-22 12:13:19 +0200

    add bash example 3

--- system-linux/src/bash实战代码02.sh
+++ system-linux/src/bash实战代码02.sh
@@ -3,13 +3,20 @@
 # find all .c or .cpp files on linux 全局查找的命令
 find . -name "*.c" -o -name "*.cpp"
 
+# 设置程序的日志文件
+logger_file=logger.txt
+if [ -f "$logger_file" ]; then
+	rm $logger_file
+fi
+touch $logger_file
+
 # $1: get the first parameter from console
 find $1 -type f -name *.hbm.xml |
 while read result
 do
-    echo $result
+    echo $result >> $logger_file
     while IFS= read -r line; do
-	   if [[ "$line" == *"<class "* || "$line" == *"<subclass "* ]]; then
+	  if [[ "$line" == *"<class "* || "$line" == *"<subclass "* ]]; then
 	      # https://www.shellunix.com/awk.html awk参考文档
 	      # 截取指定区间内的字符串
 	      class_name=`echo $line | awk -F"name=\"" '{print $2}' | awk -F "\"" '{print $1}'`
@@ -20,6 +27,9 @@ do
         # 只需要匹配第一个空格结束的位置
 		    entity_tag="entity-name=\"$class_name\" "
 		    sed -i "s/<class.*name=\"$class_name\" /&$entity_tag/g" $result
-	   fi
+	  fi
+
+	  # 这里的文件路径不能包含空格，否则会报错
+	  # $result: ambiguous redirect
     done < $result
 done
---
+++ system-linux/src/bash实战代码03.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 创建方法，$1即为方法的第一个参数
+get_greeting() {
+  echo "Hello, $1!"
+  # 方法没有直接的途径可以返回 ?
+}
+
+# 传递参数，获取方法输出的值
+STRING_VAR=$(get_greeting "chen victor")
+echo $STRING_VAR
+
+# --------------------------------------------
+
+# 截取指定字符之间的字符串
+class_name=""
+get_class_name() {
+   class_name=`echo $1 | awk -F"name=\"" '{print $2}' | awk -F "\"" '{print $1}'`
+   if [[ "$class_name" == "" ]]; then
+      echo "Error: can not find class name for the line $1"
+   fi
+}
+
+line="<class name=\"mx.test.MyClass\" >"
+echo "input line: $line"
+
+# 传递字符串变量
+get_class_name "${line}"
+echo $class_name
