73742a99705bf29688ea99a11c45cd7cc35e7257
tchen <chen2015tong@gmail.com>
2024-11-04 15:17:57 +0100

    update injection risks example

--- core-operating-system/unix/bash/http_request/demo_request.sh
+++ core-operating-system/unix/bash/http_request/demo_request.sh
@@ -4,9 +4,7 @@ EXPECTED_CODE="response ok"
 
 # 获取并判断Http请求的返回数据，测试Web接受请求的正确性
 RESULT=$(curl --request POST --header 'Content-Type: application/json' --data '{"id": 10,"question":"this is a test question"}' http://localhost:8080/chat)
-echo "-------"
 echo "$RESULT"
-echo "-------"
 
 if [[ $RESULT == $EXPECTED_CODE ]];then
    echo "Success \n"
